a9f8a1fb4abc79cf0aeaf9fd3d3471fb0b3bf1d0
zeron13570 <zeron13570@gmail.com>
2022-11-24 02:47:55 +0900

    1123

---
+++ WinAPIPong/Ball.h
@@ -0,0 +1,5 @@
+#pragma once
+#include "framework.h"
+#include "Vect.cpp"
+
+int wBall, hBall = 15;
--- WinAPIPong/WinAPIPong.cpp
+++ WinAPIPong/WinAPIPong.cpp
@@ -1,5 +1,5 @@
 ﻿// WinAPIPong.cpp : 애플리케이션에 대한 진입점을 정의합니다.
-//
+// https://austinmorlan.com/posts/pong_clone/
 
 #include "framework.h"
 #include "WinAPIPong.h"
@@ -75,7 +75,7 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
     wcex.hInstance      = hInstance;
     wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_WINAPIPONG));
     wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
-    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.hbrBackground  = (HBRUSH)GetStockObject(BLACK_BRUSH);
     wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_WINAPIPONG);
     wcex.lpszClassName  = szWindowClass;
     wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
@@ -93,12 +93,16 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
 //        이 함수를 통해 인스턴스 핸들을 전역 변수에 저장하고
 //        주 프로그램 창을 만든 다음 표시합니다.
 //
+
+int wHeight = 1280;
+int wWidth = 960;
+
 BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
    hInst = hInstance; // 인스턴스 핸들을 전역 변수에 저장합니다.
 
    HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+      CW_USEDEFAULT, 0, wHeight, wWidth, nullptr, nullptr, hInstance, nullptr);
 
    if (!hWnd)
    {
@@ -146,7 +150,8 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
         {
             PAINTSTRUCT ps;
             HDC hdc = BeginPaint(hWnd, &ps);
-            // TODO: 여기에 hdc를 사용하는 그리기 코드를 추가합니다...
+           
+
             EndPaint(hWnd, &ps);
         }
         break;
